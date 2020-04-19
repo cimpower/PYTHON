e76fb2c2c8a46df1f11aee60183c95fa1c369dff
Javi <javi@iMac-de-Javi.local>
2020-04-19 02:25:35 +0200

    Amazon 0.2

--- Python_Amazon/Amazon.py
+++ Python_Amazon/Amazon.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import smtplib
 
 headers = {"User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537.36'}
 
@@ -16,6 +17,9 @@ def echo_N():
         stock = soup.find('span',{'class':'a-size-medium a-color-state'}).get_text()
     except AttributeError:
         stock = soup.find('span',{'class':'a-size-medium a-color-success'}).get_text().strip()
+    
+    
+    
     print(title)
     print(price)
     print(stock.strip())
@@ -33,6 +37,9 @@ def echo_B():
         stock = soup.find('span',{'class':'a-size-medium a-color-state'}).get_text().strip()
     except AttributeError:
         stock = soup.find('span',{'class':'a-size-medium a-color-success'}).get_text().strip()
+
+
+
     print(title)
     print(price)
     print(stock)
@@ -44,17 +51,43 @@ def USB():
 #buscamos la etuiqueta, cogemos texto y quitamos espacios
     title_text = soup.find(id="productTitle").get_text().strip()
     title = title_text[8:27]+ ' Blanco'
-    price = soup.find(id="priceblock_ourprice").get_text()
+    price_text = soup.find(id="priceblock_ourprice").get_text()
+    price = float(price_text.replace( ",",".")[0:5])
 #para la disponibilidad compobamos si 'no stock', si falla busca 'stock'
     try:
         stock = soup.find('span',{'class':'a-size-medium a-color-state'}).get_text().strip()
     except AttributeError:
         stock = soup.find('span',{'class':'a-size-medium a-color-success'}).get_text().strip()
+
+    if(price < 15.00):
+        email()
+
     print(title)
     print(price)
     print(stock)
 
+def email():
+    server = smtplib.SMTP('smtp.gmail.com',587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
 
+    server.login('javiemg@gmail.com', 'efrgapvrokfxpjzj')
+
+    asunto = 'Ha bajado el precio!'
+    cuerpo = 'Aqui tienes el link'
+
+    msg = f"Subjet: {asunto}\n\n{cuerpo}"
+    server.sendmail(
+        'javiemg@gmail.com',
+        'j_moregon@hotmail.com',
+        msg
+    )
+    print('Se ha enviado el email!')
+
+    server.quit()
+#pasar el prcio y el link como variables
+
+USB()
 echo_N()
 echo_B()
-USB()
